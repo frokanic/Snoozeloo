28e0a8716b9ce986e14334a5e24b0c39125b79b1
frokanic <84749437+frokanic@users.noreply.github.com>
2025-02-11 17:57:57 +0200

    Add function to be able to update the timestamp

--- core/database/src/main/kotlin/com/frokanic/snoozeloo/dao/AlarmDao.kt
+++ core/database/src/main/kotlin/com/frokanic/snoozeloo/dao/AlarmDao.kt
@@ -8,12 +8,13 @@ import androidx.room.Query
 import androidx.room.Upsert
 import com.frokanic.snoozeloo.entity.AlarmEntity
 import kotlinx.coroutines.flow.Flow
+import java.time.LocalDateTime
 
 @Dao
 interface AlarmDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun saveAlarm(alarm: AlarmEntity)
+    suspend fun saveAlarm(alarm: AlarmEntity): Long
 
     @Query("SELECT * FROM alarmentity")
     fun getAllAlarms(): Flow<List<AlarmEntity>>
@@ -23,6 +24,9 @@ interface AlarmDao {
 
     @Query("UPDATE alarmentity SET isActive = NOT isActive WHERE id = :alarmId")
     suspend fun updateActiveStatus(alarmId: Int)
+
+    @Query("UPDATE alarmentity SET timeStamp = :timeStamp WHERE id = :id")
+    suspend fun updateTimeStamp(id: Int, timeStamp: LocalDateTime)
 
     @Query("DELETE FROM alarmentity WHERE id = :alarmId")
     suspend fun deleteAlarm(alarmId: Int)
