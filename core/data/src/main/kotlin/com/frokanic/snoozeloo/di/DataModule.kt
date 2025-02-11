2d3587e911248899fb5ee0c262f4696b4e6bb356
frokanic <84749437+frokanic@users.noreply.github.com>
2025-02-11 17:58:17 +0200

    Add sound and vibration to alarm ringing

--- core/data/src/main/kotlin/com/frokanic/snoozeloo/di/DataModule.kt
+++ core/data/src/main/kotlin/com/frokanic/snoozeloo/di/DataModule.kt
@@ -4,8 +4,14 @@ import com.frokanic.snoozeloo.alarm.AlarmScheduler
 import com.frokanic.snoozeloo.dao.AlarmDao
 import com.frokanic.snoozeloo.repository.AlarmsRepository
 import com.frokanic.snoozeloo.repository.AlarmsRepositoryImpl
+import com.frokanic.snoozeloo.repository.RingtoneRepository
+import com.frokanic.snoozeloo.repository.RingtoneRepositoryImpl
 import com.frokanic.snoozeloo.repository.SystemAlarmRepository
 import com.frokanic.snoozeloo.repository.SystemAlarmRepositoryImpl
+import com.frokanic.snoozeloo.repository.VibrationRepository
+import com.frokanic.snoozeloo.repository.VibrationRepositoryImpl
+import com.frokanic.snoozeloo.ringtone.RingtonePlayer
+import com.frokanic.snoozeloo.vibration.VibrationManager
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -31,5 +37,21 @@ object DataModule {
     ): SystemAlarmRepository =
         SystemAlarmRepositoryImpl(
             alarmScheduler = alarmScheduler
+        )
+
+    @Provides
+    fun provideRingtoneRepository(
+        ringtonePlayer: RingtonePlayer
+    ): RingtoneRepository =
+        RingtoneRepositoryImpl(
+            ringtonePlayer = ringtonePlayer
+        )
+
+    @Provides
+    fun provideVibrationRepository(
+        vibrationManager: VibrationManager
+    ): VibrationRepository =
+        VibrationRepositoryImpl(
+            vibrationManager = vibrationManager
         )
 }
